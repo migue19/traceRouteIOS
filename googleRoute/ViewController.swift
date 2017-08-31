099771102a1c715e894872665b437a7fca764a8a
Miguel Herrera <miguelmexicanoherrera@mac-mini-de-admincam.local>
2017-08-31 13:57:41 -0500

    Initial Commit

---
+++ googleRoute/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  googleRoute
+//
+//  Created by Miguel Mexicano Herrera on 31/08/17.
+//  Copyright © 2017 Miguel Mexicano Herrera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
